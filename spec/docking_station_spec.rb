155d955d2733d7d184cc01415058f56b66ad40d7
Thady Condon <thadycondon@hotmail.com>
2016-07-08 12:44:58 +0100

    Step 20, refactored double and added behaviours all passing.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,43 +1,35 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+
+  let(:bike) { double :bike }
   it { is_expected.to respond_to :release_bike }
   it { is_expected.to respond_to :dock }
   it { is_expected.to respond_to :bikes }
   it { is_expected.to respond_to :capacity }
 
 
-#describe '#release_bike' do
-
   it 'raises an error' do
     expect { subject.release_bike }.to raise_error 'No bike available'
   end
 
   it 'docks a bike' do
-    #bike = Bike.new
-    subject.capacity.times { subject.dock double(:bike) }
-    expect { subject.dock double(:bike) }.to raise_error 'Docking station full'
+    subject.capacity.times { subject.dock(bike) }
+    expect { subject.dock(bike)  }.to raise_error 'Docking station full'
   end
 
   it 'releases working bikes' do
-      #bike = Bike.new
-      subject.capacity.times { subject.dock double(:bike) }
+      bike = double(:working? => true)
+      subject.capacity.times { subject.dock(bike)  }
       subject.capacity.times { subject.release_bike }
       expect { subject.release_bike }.to raise_error 'No bike available'
     end
 
   it "doesn't release broken bikes" do
-    #bike = Bike.new
-    double(:bike).report
-    subject.capacity.times { subject.dock double(:bike) }
+    bike = double(:report => false, :working? => false)
+    bike.report
+    subject.capacity.times { subject.dock(bike)  }
     expect { subject.release_bike }.to raise_error 'No working bikes available'
-
-
   end
-
-            #expect(bike).to be_working
-
-
-
 
 end
